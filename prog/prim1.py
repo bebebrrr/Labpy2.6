fe3ed980b3a05d87b6eb2f90e4e806773ebc72a2
bebebrrr <kubanova04@gmail.com>
2023-12-12 13:45:59 +0300

    example

--- prog/prim1.py
+++ prog/prim1.py
@@ -5,84 +5,87 @@ import sys
 from datetime import date
 
 if __name__ == '__main__':
-    #список работников
-    workers = []
-
-    #организовать бесконечный цикл запроса команд 
-    while True:
-
-        command = input(">>> ").lower()
-
-        if command == 'exit':
-            break
-
-        elif command == 'add':
-
-            name = input("Фамилия и инициалы? ")
-            post = input("Должность? ")
-            year = int(input("Год поступления? "))
-
-            worker = {
-                'name': name,
-                'post': post,
-                'year': year,
+   # Список работников.
+   workers = []
+   # Организовать бесконечный цикл запроса команд.
+   while True:
+       # Запросить команду из терминала.
+       command = input(">>> ").lower()
+       # Выполнить действие в соответствие с командой.
+       if command == 'exit':
+           break
+       elif command == 'add':
+           # Запросить данные о работнике.
+           name = input("Фамилия и инициалы? ")
+           post = input("Должность? ")
+           year = int(input("Год поступления? "))
+           # Создать словарь.
+           worker = {
+               'name': name,
+               'post': post,
+               'year': year,
             }
-
-            workers.append(worker)
-
-            if len(workers) > 1:
-                workers.sort(key = lambda item: item.get('name', ''))
-    
-        elif command == 'list':
-            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
-                 '-' * 4,
-                 '-' * 30,
-                 '-' * 20,
-                 '-' * 8
-                )
-
-            print(line)
-            print(
-                '| {:^4} | {:^30} | {:^20} | {:^8}'.format(
-                    "№",
-                    "Ф.И.О.",
+           # Добавить словарь в список.
+           workers.append(worker)
+           # Отсортировать список в случае необходимости.
+           if len(workers) > 1:
+               workers.sort(key=lambda item: item.get('name', ''))
+       elif command == 'list':
+           # Заголовок таблицы.
+           line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+               '-' * 4,
+               '-' * 30,
+               '-' * 20,
+               '-' * 8
+            )
+           print(line)
+           print(
+               '| {:^4} | {:^30} | {:^20} | {:^8} |'.format(
+                   "№",
+                   "Ф.И.О.",
                     "Должность",
-                    "Год"
+                   "Год"
                 )
             )
-
-            for idx, worker in enumerate(workers, 1):
-                print(
-                    '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
-                        idx,
-                        worker.get('name', ''),
-                        worker.get('post', ''),
-                        worker.get('year', 0)
+           print(line)
+           # Вывести данные о всех сотрудниках.
+           for idx, worker in enumerate(workers, 1):
+               print(
+                   '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
+                       idx,
+                       worker.get('name', ''),
+                       worker.get('post', ''),
+                       worker.get('year', 0)
                     )
                 )
-            
-            print(line)
-        elif command.startswith('select '):
-            today = date.today()
-
-            parts = command.split(' ', maxsplit=1)
-            period = int(parts[1])
-            count = 0
-            for worker in workers:
-                if today.year - worker.get('year', today.year) >= period:
-                    count += 1
-                    print(
-                        '{:>4}: {}'.format(count, worker.get('name', ''))
+           print(line)
+       elif command.startswith('select '):
+           # Получить текущую дату.
+           today = date.today()
+           # Разбить команду на части для выделения номера года.
+           parts = command.split(' ', maxsplit=1)
+           # Получить требуемый стаж.
+           period = int(parts[1])
+           # Инициализировать счетчик.
+           count = 0
+           # Проверить сведения работников из списка.
+           for worker in workers:
+               if today.year - worker.get('year', today.year) >= period:
+                   count += 1
+                   print(
+                       '{:>4}: {}'.format(count, worker.get('name', ''))
                     )
-            if count == 0:
-                print("Работники с заданным стажем не найдены.")
-        elif command == 'help':
-            print("Список команд:\n")
-            print("add - добавить работника;")
-            print("list - вывести список работников;")
-            print("select <стаж> - запросить работников со стажем;")
-            print("help - отобразить справку;")
-            print("exit - завершить работу с программой.")
-        else:
-            print(f"Неизвестная команда {command}", file=sys.stderr)
+           # Если счетчик равен 0, то работники не найдены.
+           if count == 0:
+               print("Работники с заданным стажем не найдены.")
+       elif command == 'help':
+           # Вывести справку о работе с программой.
+           print("Список команд:\n")
+           print("add - добавить работника;")
+           print("list - вывести список работников;")
+           print("select <стаж> - запросить работников со стажем;")
+           print("help - отобразить справку;")
+           print("exit - завершить работу с программой.")
+       else:
+           print(f"Неизвестная команда {command}", file=sys.stderr)
            
